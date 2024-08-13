4d4bedf6fdb0900c6a152a089c81fc74623714c0
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-08-13 20:15:10 -0300

    feat(): Tela SettingsPage;

--- app/src/main/java/com/example/criptonita/ui/SettingsPage.kt
+++ app/src/main/java/com/example/criptonita/ui/SettingsPage.kt
@@ -2,16 +2,23 @@ package com.example.criptonita.ui
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -31,19 +38,58 @@ import com.example.criptonita.R
 @Preview
 @Composable
 fun SettingsPage() {
-    Column(
+    Scaffold(
+
+    ) { innerPadding ->
+        LazyColumn(
+            contentPadding = innerPadding,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            item {
+                SettingsItem(title = "Moeda Base") {
+                    // Navegue para a tela de configuração de Moeda Base
+                }
+            }
+            item {
+                SettingsItem(title = "Idioma") {
+                    // Navegue para a tela de configuração de Idioma
+                }
+            }
+            item {
+                SettingsItem(title = "Tema") {
+                    // Navegue para a tela de configuração de Tema
+                }
+            }
+            item {
+                SettingsItem(title = "Informações da conta") {
+                    // Navegue para a tela de Informações da conta
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SettingsItem(title: String, onClick: () -> Unit) {
+    Row(
         modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.teal_700))
-            .wrapContentSize(Alignment.Center)
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = "Settings",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+            text = title,
+            modifier = Modifier.weight(1f),
+            fontSize = 18.sp,
+            color = Color.Black
+        )
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowRight,
+            contentDescription = "Arrow",
+            tint = Color.Gray
         )
     }
 }
